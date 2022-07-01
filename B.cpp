103dcce9a5fff1785bfe864800873882212c5a88
Nourhan-Atef-Hossam-El-Deen <91796430+Nourhaan-Atef@users.noreply.github.com>
2022-07-01 21:03:49 +0200

    Add files via upload

---
+++ B.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a;
+	long long b;
+	char d;
+	float e;
+	double f;
+	cin>>a>>b>>d>>e>>f;
+	cout<<a<<"\n"<<b<<"\n"<<d<<"\n"<<e<<"\n"<<f<<endl;
+}
+
