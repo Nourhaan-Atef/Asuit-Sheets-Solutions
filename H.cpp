2a623dfd26bcb28b49a17b0f18856c0ade7dc1ce
Nourhan-Atef-Hossam-El-Deen <91796430+Nourhaan-Atef@users.noreply.github.com>
2022-07-05 10:11:25 +0200

    H Problem

---
+++ H.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+ double n,m;
+ cin>>n>>m;
+ cout<<"floor "<<n<<" / "<<m<<" = "<<floor(n/m)<<endl;
+ cout<<"ceil "<<n<<" / "<<m<<" = "<<ceil(n/m)<<endl;
+ cout<<"round "<<n<<" / "<<m<<" = "<<round(n/m);
+
+}
